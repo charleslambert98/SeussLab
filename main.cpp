1760c360c91ddcb8720d9a59a678ab189aa0b811
charleslambert98 <clambert@udel.edu>
2018-11-18 10:32:44 -0500

    Added Results.txt

--- main.cpp
+++ main.cpp
@@ -14,10 +14,10 @@ using namespace std;
 
 int main(){
     //srand(time_t(NULL));
-    makeSeuss("/Users/clambert/Documents/Fall 2018/CISC220/Lab 5/DrSeuss/DrSeuss/DrSeuss.txt", "/Users/clambert/Documents/Fall 2018/CISC220/Lab 5/DrSeuss/DrSeuss/calcHash1_collhash1.txt", 0, 0);
-    makeSeuss("/Users/clambert/Documents/Fall 2018/CISC220/Lab 5/DrSeuss/DrSeuss/DrSeuss.txt", "/Users/clambert/Documents/Fall 2018/CISC220/Lab 5/DrSeuss/DrSeuss/calcHash2_collhash1.txt", 1, 0);
-    makeSeuss("/Users/clambert/Documents/Fall 2018/CISC220/Lab 5/DrSeuss/DrSeuss/DrSeuss.txt", "/Users/clambert/Documents/Fall 2018/CISC220/Lab 5/DrSeuss/DrSeuss/calcHash1_collHash2.txt", 0, 1);
-    makeSeuss("/Users/clambert/Documents/Fall 2018/CISC220/Lab 5/DrSeuss/DrSeuss/DrSeuss.txt", "/Users/clambert/Documents/Fall 2018/CISC220/Lab 5/DrSeuss/DrSeuss/calcHash2_collHash2.txt", 1, 1);
+    makeSeuss("DrSeuss.txt", "calcHash1_collhash1.txt", 0, 0);
+    makeSeuss("DrSeuss.txt", "calcHash2_collhash1.txt", 1, 0);
+    makeSeuss("DrSeuss.txt", "calcHash1_collHash2.txt", 0, 1);
+    makeSeuss("DrSeuss.txt", "calcHash2_collHash2.txt", 1, 1);
 
     
     return 0;
